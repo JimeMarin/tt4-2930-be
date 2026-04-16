6ecf20785209ba0959ebad0a16c4741b876b4cf5
Jimena <jimemarinp@gmail.com>
2026-04-16 11:12:34 -0400

    websocket in frontend

---
+++ frontend/src/app/core/realtime.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Socket, io } from 'socket.io-client';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class RealTimeService {
+  private socket: Socket | null = null;
+
+  connect(onTaskCreated: () => void): void {
+    if(this.socket?.connected){
+      return;
+    }
+
+    if(this.socket){
+      this.socket.connect();
+    }
+
+    this.socket = io(environment.apiUrl, {
+      transports: ['websocket'],
+    });
+
+    this.socket.on("task:created", onTaskCreated);
+  }
+
+  connectDeleteTask(onTaskDeleted: () => void){
+    if(this.socket?.connected){
+      return;
+    }
+
+    if(this.socket){
+      this.socket.connect();
+    }
+
+    this.socket = io(environment.apiUrl, {
+      transports: ['websocket'],
+    });
+
+    this.socket.on("task:deleted", onTaskDeleted);
+  }
+
+  connectUpdateTask(onTaskUpdated: () => void){
+    if(this.socket?.connected){
+      return;
+    }
+
+    if(this.socket){
+      this.socket.connect();
+    }
+
+    this.socket = io(environment.apiUrl, {
+      transports: ['websocket'],
+    });
+
+    this.socket.on("task:updated", onTaskUpdated);
+  }
+  
+  disconnect(): void {
+    this.socket?.disconnect();
+    this.socket?.off("task:created");
+  }
+}
+
